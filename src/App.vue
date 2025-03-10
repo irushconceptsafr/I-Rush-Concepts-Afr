98a49b667e8cc9e28d290cf18389859147461f58
Mithibe Irungu <Kenmithibe@gmail.com>
2025-03-10 11:35:39 +0300

    initial commit: Irush website

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+  </header>
+  <main></main>
+</template>
+
+<style scoped>
+template{
+  height: 100vh;
+}
+
+header{
+  position: fixed;
+  right: 0;
+  height: 100vh;
+  width: 80px;
+  border: 2px solid;
+}
+
+main{
+  width: calc(100vw - 100px);
+  border: 2px solid #aaa;
+  height: 100vh;
+}
+</style>
