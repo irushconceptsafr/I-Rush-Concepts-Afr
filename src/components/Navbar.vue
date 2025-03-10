0c8ed823c203b4a5fd6e62f6320b0afeb4b27c0d
Mithibe Irungu <Kenmithibe@gmail.com>
2025-03-10 12:17:59 +0300

    Added Router 4

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from "vue";
+</script>
+
+<template>
+    <nav>
+        <div class="logo"></div>
+        <div class="links">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/services">Services</RouterLink>
+            <RouterLink to="/Contact">Contact</RouterLink>
+        </div>
+    </nav>
+</template>
